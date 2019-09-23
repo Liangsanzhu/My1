970ad66de13e2bff0432bcb137c37475e41d73ec
RoseL <lbaoyou0403@163.com>
2019-09-23 14:52:02 +0800

    cfd s
    fds

---
+++ Assets/Scenes/Scripts/EnemyMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region PUBLIC_VAR
+    public int health;
+    public bool hurted = false;
+    #endregion
+
+    #region PRIVATE_VAR
+    SpriteRenderer body;
+    Animator anim;
+    bool isDead = false;
+    int waitForDestroy = 1;
+    bool colorChanged = false;
+    #endregion
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("Hurt");
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isDead", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hurted == true)
+        {
+            StartCoroutine(changeColor(new Vector4(1, 0.3f, 0.3f, 1)));
+            anim.SetTrigger("Hurt");
+            anim.SetBool("isIdle", false);
+            hurted = false;
+        }
+        else if (hurted == false && isDead == false)
+        {
+            anim.ResetTrigger("Hurt");
+            anim.SetBool("isIdle", true);
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+        }
+
+        if (health < 0)
+        {
+            isDead = true;
+            body.color = new Color(1, 1, 1, 1);
+            anim.SetTrigger("Hurt");
+            StartCoroutine(waitToBeDestory());
+            anim.SetBool("isIdle", false);
+        }
+
+        if (colorChanged)
+        {
+            body.color = new Color(1, 1, 1, 1);
+            colorChanged = false;
+        }
+
+    }
+
+    IEnumerator waitToBeDestory()
+    {
+        yield return new WaitForSeconds(waitForDestroy);
+        Destroy(gameObject);
+    }
+
+    IEnumerator changeColor(Vector4 color)
+    {
+        body.color = new Color(color.x, color.y, color.z, color.w);
+        yield return new WaitForSeconds(0.5f);
+        colorChanged = true;
+    }
+}
---
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region PUBLIC_VAR
+
+    #endregion
+
+    #region PRIVATE_VAR
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRadius;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private int damage;
+    Animator anim;
+    bool damageMade;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isAttacking", false);
+        damageMade = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetBool("isAttacking", true);
+            damageMade = true;
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("attack") == true && damageMade == true)
+        {
+
+
+            Collider2D[] hittedObjects = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius);
+            if (hittedObjects.Length > 0)
+            {
+                int check = 0;
+                for (int i = 0; i < hittedObjects.Length; i++)
+                {
+                    if (hittedObjects[i].gameObject != gameObject)
+                    {
+
+                        check = 1;
+                        EnemyMovement enemy = hittedObjects[i].gameObject.GetComponent<EnemyMovement>();
+                        if (enemy != null)
+                        {
+                            enemy.health -= damage;
+                            enemy.hurted = true;
+                        }
+                        Debug.Log(enemy.health);
+                    }
+                }
+                if (check == 1)
+                    damageMade = false;
+
+            }
+        }
+
+        float movement = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        if (movement != 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("attack") == false)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isIdle", false);
+
+
+            if (movement > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+            else if (movement < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+        }
+    
+        else if (movement == 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("attack") == false)
+        {
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isIdle", true);
+ 
+        }
+
+
+
+    }
+
+
+}
